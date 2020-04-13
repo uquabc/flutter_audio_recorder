67f75972f2e6a60d303dc573abf291e9e04c5608
cnting <cnting@CTMacBook-Pro.local>
2020-04-13 16:15:59 +0800

    fix android bug

--- android/src/main/java/com/zeno/flutter_audio_recorder/FlutterAudioRecorderPlugin.java
+++ android/src/main/java/com/zeno/flutter_audio_recorder/FlutterAudioRecorderPlugin.java
@@ -354,7 +354,7 @@ public class FlutterAudioRecorderPlugin implements MethodCallHandler, PluginRegi
     header[29] = (byte) ((byteRate >> 8) & 0xff);
     header[30] = (byte) ((byteRate >> 16) & 0xff);
     header[31] = (byte) ((byteRate >> 24) & 0xff);
-    header[32] = (byte) (1); // block align
+    header[32] = (byte) (channels * 16 / 8); // block align
     header[33] = 0;
     header[34] = RECORDER_BPP; // bits per sample
     header[35] = 0;
